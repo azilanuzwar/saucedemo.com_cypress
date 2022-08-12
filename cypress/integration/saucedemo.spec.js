26d1a04baa77a30bd04ed01a9aaab71e17cc67c7
azilanuzwar <user.azilanuzwar@gmail.com>
2022-08-12 20:56:59 +0700

    write positive testcasae

--- cypress/integration/saucedemo.spec.js
+++ cypress/integration/saucedemo.spec.js
@@ -1,5 +1,35 @@
 /// <reference types="cypress" />
 
-it('should navigate to the saucedemo.com', () => {
-    cy.visit('https://www.saucedemo.com')
+it('Positive case from login until finish', () => {
+    cy.visit('https://www.saucedemo.com/')
+
+    cy.get('[data-test=username]').type('standard_user')
+
+    cy.get('[data-test=password]').type('secret_sauce')
+
+    cy.get('[data-test=login-button]').click()
+
+    ///at the home page:
+    cy.get('[data-test=add-to-cart-sauce-labs-backpack]').click()
+
+    cy.get('.shopping_cart_badge').click()
+
+    ///at the cart page:
+    cy.get('[data-test=checkout]').click()
+
+    ///at the check-out-step1-page:
+    cy.get('[data-test=firstName]').type('Azila')
+
+    cy.get('[data-test=lastName]').type('Nuzwar')
+
+    cy.get('[data-test=postalCode]').type('12345')
+
+    cy.get('[data-test=continue]').click()
+
+    ///at the check-out-step2-page:
+    cy.get('[data-test=finish]').click()
+
+    ///at the checkout-complete page:
+    cy.get('.complete-header').should('be.visible')
+
 })
