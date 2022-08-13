e4ec0a4c222de40e4780c39e690f51636bab54ae
azilanuzwar <user.azilanuzwar@gmail.com>
2022-08-13 10:44:47 +0700

    add test script for login page

--- cypress/integration/saucedemo.spec.js
+++ cypress/integration/saucedemo.spec.js
@@ -1,35 +1,75 @@
 /// <reference types="cypress" />
 
-it('Positive case from login until finish', () => {
-    cy.visit('https://www.saucedemo.com/')
+describe ('filtering', function(){
 
-    cy.get('[data-test=username]').type('standard_user')
+    beforeEach(() => {
+        cy.visit('https://www.saucedemo.com/')
+    })
 
-    cy.get('[data-test=password]').type('secret_sauce')
+    it('Positive case from login until finish', () => {    
+        cy.get('[data-test=username]').type('standard_user')
+        cy.get('[data-test=password]').type('secret_sauce')
+        cy.get('[data-test=login-button]').click()
+    
+        ///at the home page:
+        cy.get('[data-test=add-to-cart-sauce-labs-backpack]').click()
+    
+        cy.get('.shopping_cart_badge').click()
+    
+        ///at the cart page:
+        cy.get('[data-test=checkout]').click()
+    
+        ///at the check-out-step1-page:
+        cy.get('[data-test=firstName]').type('Azila')
+    
+        cy.get('[data-test=lastName]').type('Nuzwar')
+    
+        cy.get('[data-test=postalCode]').type('12345')
+    
+        cy.get('[data-test=continue]').click()
+    
+        ///at the check-out-step2-page:
+        cy.get('[data-test=finish]').click()
+    
+        ///at the checkout-complete page:
+        cy.get('.complete-header').should('be.visible')
+    })
 
-    cy.get('[data-test=login-button]').click()
+    it('Login with Valid Username and Invalid Pass', () => {
+        cy.get('[data-test=username]').type('standard_user')
+        cy.get('[data-test=password]').type('passalah')
+        cy.get('[data-test=login-button]').click()
+        cy.get('[data-test=error]').should('be.visible')
+    })
 
-    ///at the home page:
-    cy.get('[data-test=add-to-cart-sauce-labs-backpack]').click()
+    it('Login with Invalid Username and Valid Pass', () => {
+        cy.get('[data-test=username]').type('usernamesalah')
+        cy.get('[data-test=password]').type('secret_sauce')
+        cy.get('[data-test=login-button]').click()
+        cy.get('[data-test=error]').should('be.visible')
+    })
 
-    cy.get('.shopping_cart_badge').click()
+    it('Login with Invalid Username and Invalid Pass', () => {
+        cy.get('[data-test=username]').type('usernamesalah')
+        cy.get('[data-test=password]').type('passalah')
+        cy.get('[data-test=login-button]').click()
+        cy.get('[data-test=error]').should('be.visible')
+    })
 
-    ///at the cart page:
-    cy.get('[data-test=checkout]').click()
+    it('Login with Valid Username and Blank Pass', () => {
+        cy.get('[data-test=username]').type('standard_user')
+        cy.get('[data-test=login-button]').click()
+        cy.contains('[data-test=error]', 'Password is required')
+    })
 
-    ///at the check-out-step1-page:
-    cy.get('[data-test=firstName]').type('Azila')
+    it('Login with Blank Username and Valid Pass', () => {
+        cy.get('[data-test=password]').type('secret_sauce')
+        cy.get('[data-test=login-button]').click()
+        cy.contains('[data-test=error]', 'Username is required')
+    })
 
-    cy.get('[data-test=lastName]').type('Nuzwar')
-
-    cy.get('[data-test=postalCode]').type('12345')
-
-    cy.get('[data-test=continue]').click()
-
-    ///at the check-out-step2-page:
-    cy.get('[data-test=finish]').click()
-
-    ///at the checkout-complete page:
-    cy.get('.complete-header').should('be.visible')
-
+    it.only('Login with Blank Username and Blank Pass', () => {
+        cy.get('[data-test=login-button]').click()
+        cy.contains('[data-test=error]', 'Username is required')
+    })
 })
